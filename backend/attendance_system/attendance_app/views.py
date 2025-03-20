3cd83baa13db3359c0e5a771ad9a34d43b071970
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-20 23:54:03 +0530

    add backend

--- backend/attendance_system/attendance_app/views.py
+++ backend/attendance_system/attendance_app/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
+from django.shortcuts import render 
+from django.http import HttpResponse 
+from django.template import loader
 
 
 def home(req):
     return HttpResponse('Hello world')
 
-
+def members(request):
+   template = loader.get_template('myfirst.html')
+   return HttpResponse(template.render())

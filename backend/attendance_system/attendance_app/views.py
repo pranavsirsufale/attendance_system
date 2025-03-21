a58288b8fc12a7235551b8f831fe33a3b1a3c020
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-21 21:16:41 +0530

    add python concepts

--- backend/attendance_system/attendance_app/views.py
+++ backend/attendance_system/attendance_app/views.py
@@ -1,11 +1,15 @@
 # from django.shortcuts import render 
 from django.http import HttpResponse 
 from django.template import loader
-
+from .models import Student
 
 def home(req):
     return HttpResponse('Hello world')
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Student.objects.all().values()
+    template = loader.get_template('all_students.html')
+    context = {
+        'students':mymembers
+    }
+    return HttpResponse(template.render(context , request))

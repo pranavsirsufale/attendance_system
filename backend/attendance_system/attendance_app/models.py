01d11e4f3894586d152455654eb2eac43193d800
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-21 23:37:25 +0530

    add python concepts

--- backend/attendance_system/attendance_app/models.py
+++ backend/attendance_system/attendance_app/models.py
@@ -9,4 +9,10 @@ class Student(models.Model):
     phone = models.IntegerField(null = True)
     joined_date = models.DateField(null = True)
 
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+    
+
+
+
     
--- backend/attendance_system/attendance_system/settings.py
+++ backend/attendance_system/attendance_system/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--s6+hqoscj$dfof=z5c^m)77f3xfuvrlrii+2v=7+84=+_t)f1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

bf5db1a34e17c5025c986393fa5496aa118ea741
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-21 19:41:44 +0530

    add python concepts

--- pythoncode/multithreding.py
+++ pythoncode/multithreding.py
@@ -6,13 +6,33 @@ def func(seconds):
     print(f'sleeping for {seconds} seconds')
     time.sleep(seconds)
 
+# time1 = time.perf_counter()
 
-func(4)
-func(3)
-func(1)
+# normal code without threads 
+# func(4)
+# func(3)
+# func(1)
+
+# time2 = time.perf_counter()
+# print(f'total time taken by normal without threads {time2 - time1}')
 
 
 
 
+time3 = time.perf_counter()
+# same code using thread
+t1 = threading.Thread(target=func , args=[4])
+t2 = threading.Thread(target=func , args=[2])
+t3 = threading.Thread(target=func , args=[1])
+
+t1.start() 
+t2.start()
+t3.start()
 
 
+t1.join()
+t2.join()
+t3.join()
+
+time4 = time.perf_counter()
+print(f'total time taken by the code with threads are {time4 - time3}')

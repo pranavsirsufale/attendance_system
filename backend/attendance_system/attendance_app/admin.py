86a779d5bd9b0c1ea72580fd41fe1629653392b6
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-22 07:50:07 +0530

    add django project

--- backend/attendance_system/attendance_app/admin.py
+++ backend/attendance_system/attendance_app/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import Student
-
-# Register your models here.
-class AdminStudent(admin.ModelAdmin):
-    list_display = ('firstname' , 'lastname' , 'joined_date')
-
-
-
-admin.site.register(Student, AdminStudent)

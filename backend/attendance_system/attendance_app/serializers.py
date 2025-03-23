a23bad51ffb8017a4bdc1b7147eb1d460956246a
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-23 11:33:03 +0530

    update database structure

--- backend/attendance_system/attendance_app/serializers.py
+++ backend/attendance_system/attendance_app/serializers.py
@@ -1,1 +1,15 @@
-from rest_framwork 
+from rest_framework import serializers 
+from .models import Session , Student , Attendance
+
+class SessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Session 
+        fields = ['id','timetable','date', 'status']
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = ['id', 'student' , 'session' , 'status','timestamp' ,'recorded_by' ]
+
+
+    

cc44ec7df9a7130f507a9548417bc260bf62b57a
ROHAN MAGAR <97357007+RohanMagar7@users.noreply.github.com>
2025-06-16 17:17:42 +0530

    Delete null directory

--- null/frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: 'class', // Use 'class' for dark mode
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
